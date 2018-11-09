5ae67c8cb7ee46fdb59010a1517a94da486c1511
Unknown <dwiprasetyo.an@gmail.com>
2018-11-09 14:53:12 +0100

    Add basic DBScan functionality

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
 import util.InputReader;
-
+import core.DBScan;
 import java.io.FileNotFoundException;
 
 public class Main {
 
     public static void main(String[] args) {
-        InputReader inputReader = new InputReader();
-        try {
-            // Reading input csv
-            String[][] dataFrame = inputReader.readCsv("/Users/dpanugroho/Downloads/iris.data.txt");
-            System.out.print(dataFrame[1][1]);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+//        InputReader inputReader = new InputReader();
+//        try {
+//            // Reading input csv
+//            String[][] dataFrame = inputReader.readCsv("/Users/dpanugroho/Downloads/iris.data.txt");
+//            System.out.print(dataFrame[1][4]);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+        double[][] sampleDataset = new double[100][5];
+        for (int i = 0; i < sampleDataset.length; i++) {
+            for (int j = 0; j < sampleDataset[i].length; j++) {
+                sampleDataset[i][j] = Math.random()*10;
+            }
+        }
+
+        // TODO: Verify the correctness of this clustering result
+        DBScan mDBScan = new DBScan(sampleDataset, 4, 5);
+        int[] labels = mDBScan.Scan();
+        for(int i=0; i<labels.length; i++){
+            System.out.println(labels[i]);
         }
     }
 }
--- src/core/DBScan.java
+++ src/core/DBScan.java
@@ -1,15 +1,100 @@
 package core;
 
+import java.util.ArrayList;
+
+// TODO: Write docs
 public class DBScan {
-    public DBScan() {
+    double[][] D ;
+    int[] labels;
+    double eps;
+    int minPts;
+
+    public DBScan(double[][] D, double eps, int minPts) {
+        // Initialize labels
+        this.D = D;
+        this.labels = new int[D.length];
+        this.eps = eps;
+        this.minPts = minPts;
     }
 
-    private void Scan(){
-        // TODO: Do stuff
+    // TODO: Write docs
+    private static double l2(double[] source, double[] target){
+        // TODO: Check to make sure source and target has the same dimension
+
+        // Calculate the distance
+        float tmp = 0;
+        for (int i=0; i<source.length; i++){
+            tmp+= Math.pow((source[i] - target[i]),2);
+        }
+        return Math.sqrt(tmp);
+    }
+
+    // TODO: Write docs
+    public int[] Scan(){
+
+
+        // c is ID if current cluster
+        int c = 0;
+
+        // TODO: Comments what happening here
+        for (int p=0;p<this.D.length;p++){
+            if(!(this.labels[p]==0)){
+                continue;
+            }
+            ArrayList<Integer> neighborPts = regionQuery(p);
+            //System.out.println(neighborPts.size());
+            if (neighborPts.size() < this.minPts){
+                this.labels[p] = -1;
+            } else {
+                c += 1;
+                expandCluster(c, p, neighborPts);
+            }
+
+        }
+        return labels;
+    }
+
+    // TODO: Write docs
+    private void expandCluster(int c, int p, ArrayList<Integer> neighborPts){
+        this.labels[p] = c;
+
+        int i = 0;
+        while (i<neighborPts.size()){
+            int Pn = neighborPts.get(i);
+
+            if(this.labels[Pn] == -1){
+                labels[Pn] = c;
+            } else if (this.labels[Pn] == 0){
+                this.labels[Pn] = c;
+
+                ArrayList<Integer> PnNeighborPts = regionQuery(Pn);
+
+                if(PnNeighborPts.size() >= this.minPts){
+                    neighborPts.addAll(PnNeighborPts);
+                }
+            }
+            i += 1;
+        }
+
 
     }
 
-    private void expandCluster(){
-        // TODO: Do stuff
+    /**
+     * Find all points in dataset `D` within distance `eps` of point `P`.
+     *
+     * @param P index of core point
+     * @return
+     */
+    private ArrayList<Integer> regionQuery(int P){
+        ArrayList<Integer> neighbors = new ArrayList<>();
+
+        for(int i=0; i<this.D.length; i++){
+            //System.out.println(l2(this.D[P], this.D[i]));
+            if(l2(this.D[P], this.D[i]) < this.eps){
+                neighbors.add(i);
+            }
+        }
+
+        return neighbors;
     }
 }
