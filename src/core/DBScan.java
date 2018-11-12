0e85641abbf2d18c2f60a2675c22bef9b8428a4a
Haydar Ali Ismail <haydar_ai@yahoo.com>
2018-11-12 21:19:59 +0100

    Improve readability and add comments

--- src/core/DBScan.java
+++ src/core/DBScan.java
@@ -29,54 +29,57 @@ public class DBScan {
         return Math.sqrt(tmp);
     }
 
-    // TODO: Write docs
+    /**
+     * Main DBScan process, iterating through all points in dataset
+     * @return labels for each points
+     */
     public int[] Scan() {
-
-
         // c is ID if current cluster
         int c = 0;
 
-        // TODO: Comments what happening here
+        // Go through all points in dataset
         for (int p = 0; p < this.D.length; p++) {
-            if (!(this.labels[p] == 0)) {
-                continue;
+            // 0: not visited yet, -1: doesn't belong to any cluster, other: belong to the specified cluster
+            if (this.labels[p] == 0) {
+                ArrayList<Integer> neighborPts = regionQuery(p);
+                //System.out.println(neighborPts.size());
+                if (neighborPts.size() < this.minPts) {
+                    this.labels[p] = -1; // this point doesn't have enough neighbor to be considered a cluster
+                } else {
+                    c++; // change to the next cluster
+                    expandCluster(c, p, neighborPts);
+                }
             }
-            ArrayList<Integer> neighborPts = regionQuery(p);
-            //System.out.println(neighborPts.size());
-            if (neighborPts.size() < this.minPts) {
-                this.labels[p] = -1;
-            } else {
-                c += 1;
-                expandCluster(c, p, neighborPts);
-            }
-
         }
         return labels;
     }
 
-    // TODO: Write docs
+    /**
+     * From current cluster, expand cluster with neighbor points
+     * @param c cluster id
+     * @param p a point
+     * @param neighborPts neighbor points of p
+     */
     private void expandCluster(int c, int p, ArrayList<Integer> neighborPts) {
-        this.labels[p] = c;
+        this.labels[p] = c; // mark p as visited and add p to cluster c
 
-        int i = 0;
-        while (i < neighborPts.size()) {
+        for (int i = 0; i < neighborPts.size(); i++) {
             int Pn = neighborPts.get(i);
 
+            // add P' to cluster c is it's not member of any cluster yet
             if (this.labels[Pn] == -1) {
-                labels[Pn] = c;
+                labels[Pn] = c; // mark P' as visited and add to cluster c
             } else if (this.labels[Pn] == 0) {
-                this.labels[Pn] = c;
+                this.labels[Pn] = c; // mark P' as visited and add to cluster c
 
                 ArrayList<Integer> PnNeighborPts = regionQuery(Pn);
 
                 if (PnNeighborPts.size() >= this.minPts) {
+                    // TODO: clarify with dpanugroho what he is trying to do
                     neighborPts.addAll(PnNeighborPts);
                 }
             }
-            i += 1;
         }
-
-
     }
 
     /**
