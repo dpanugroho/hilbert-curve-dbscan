ca4d85e541b3ad416c40fea5f3e1dd4351d14dfa
codingamanda <45100379+codingamanda@users.noreply.github.com>
2018-11-18 01:10:28 +0100

    Add functions to HilbertCurve.java

--- src/hilbert/HilbertCurve.java
+++ src/hilbert/HilbertCurve.java
@@ -39,6 +39,33 @@ public class HilbertCurve {
         Preconditions.checkArgument(point.length == dimensions);
         return toIndex(transposedIndex(bits, point));
     }
+    
+    public long[] point(BigInteger index) {
+        Preconditions.checkNotNull(index);
+        Preconditions.checkArgument(index.signum() != -1, "index cannot be negative");
+        return transposedIndexToPoint(bits, transpose(index));
+    }
+
+    public long[] point(long index) {
+        return point(BigInteger.valueOf(index));
+    }
+    
+    long[] transpose(BigInteger index) {
+        long[] x = new long[dimensions];
+        transpose(index, x);
+        return x;
+    }
+
+    private void transpose(BigInteger index, long[] x) {
+        byte[] b = index.toByteArray();
+        for (int idx = 0; idx < 8 * b.length; idx++) {
+            if ((b[b.length - 1 - idx / 8] & (1L << (idx % 8))) != 0) {
+                int dim = (length - idx - 1) % dimensions;
+                int shift = (idx / dimensions) % bits;
+                x[dim] |= 1L << shift;
+            }
+        }
+    }
 
     BigInteger toIndex(long... transposedIndex) {
         byte[] b = new byte[length];
@@ -85,6 +112,35 @@ public class HilbertCurve {
         for (i = 0; i < n; i++)
             x[i] ^= t;
 
+        return x;
+    }
+    
+    static long[] transposedIndexToPoint(int bits, long... x) {
+        final long N = 2L << (bits - 1);
+        // Note that x is mutated by this method (as a performance improvement
+        // to avoid allocation)
+        int n = x.length; // number of dimensions
+        long p, q, t;
+        int i;
+        // Gray decode by H ^ (H/2)
+        t = x[n - 1] >> 1;
+        // Corrected error in Skilling's paper on the following line. The
+        // appendix had i >= 0 leading to negative array index.
+        for (i = n - 1; i > 0; i--)
+            x[i] ^= x[i - 1];
+        x[0] ^= t;
+        // Undo excess work
+        for (q = 2; q != N; q <<= 1) {
+            p = q - 1;
+            for (i = n - 1; i >= 0; i--)
+                if ((x[i] & q) != 0L)
+                    x[0] ^= p; // invert
+                else {
+                    t = (x[0] ^ x[i]) & p;
+                    x[0] ^= t;
+                    x[i] ^= t;
+                }
+        } // exchange
         return x;
     }
 }
